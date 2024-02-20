21c7c9d877fdc2b85dc08b85440fe5d9503f1c19
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:42:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть довжину лінії: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть символ заповнювач: ");
+        char fillChar = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Введіть напрямок лінії (г - горизонтальна, в - вертикальна): ");
+        char direction = Convert.ToChar(Console.ReadLine());
+
+        
+        if (direction == 'г')
+        {
+            
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(fillChar);
+            }
+            Console.WriteLine(); 
+        }
+        else if (direction == 'в')
+        {
+            
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(fillChar);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неправильно введений напрямок лінії.");
+        }
+    }
+}
+
